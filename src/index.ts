1bb1308be755f355f612e0f842e0d318f023d60f
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-11-27 02:51:09 -0800

    Set up CLI boilerplate

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+const pkg = require('../package.json');
+
+const cli = new Command();
+
+cli
+  .version(`@delgada/cli: ${pkg.version}`)
+  .option('-c, --config <path>', 'set config path', './delgada.config.js');
+
+cli
+  .command('build')
+  .description('compile delgada source code')
+  .option('-e, --entry <path>', 'set entrypoint path', './src/index.html')
+  .option('-b, --build <path>', 'set build path', './build')
+  .action((options) => {
+    console.log('Compiling source code...');
+  });
+
+cli.parse(process.argv);
+
+process.on('uncaughtException', function (err) {
+  console.error(err);
+  process.exit(1);
+});
