f1ab5a4ee8cf96d10ff88a21ae0e66b244441f87
Hawk Ticehurst <hawkticehurst@gmail.com>
2022-01-03 22:37:20 -0800

    Add an --assets flag to build command that will copy an assets folder to the build directory

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Command } from 'commander';
 import { compile } from '@delgada/core';
+import { copyDir } from './utils/copyDir';
 
 const pkg = require('../package.json');
 
@@ -12,10 +13,20 @@ cli
   .description('compile delgada source code')
   .option('-e, --entry <path>', 'set entrypoint directory', './src')
   .option('-b, --build <path>', 'set build directory', './build')
+  .option('-a, --assets <path>', 'set assets directory', './src/assets')
   .action((options) => {
     const entryDir: string = options.entry;
     const buildDir: string = options.build;
+    const assetsDir: string = options.assets;
+
+    // Compile and build source code
     compile(entryDir, buildDir);
+
+    // If an asset directory path was given, copy
+    // its contents into the build directory
+    if (assetsDir) {
+      copyDir(assetsDir, buildDir);
+    }
   });
 
 cli.parse(process.argv);
